12c096e67b040852183e4e9ac44296eac479a60b
iiaaan <enhaoliu14@gmail.com>
2025-11-06 20:21:44 -0700

    Added basic entrant activity that allow entrants to view list of activities and event details

---
+++ EntrantScreenActivity/app/src/main/java/com/example/entrantscreenactivity/adapters/EventAdapter.java
@@ -0,0 +1,60 @@
+package com.example.entrantscreenactivity.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.*;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.entrantscreenactivity.R;
+import com.example.entrantscreenactivity.models.Event;
+import com.example.entrantscreenactivity.views.EventDetailsActivity;
+
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Event> eventList;
+
+    public EventAdapter(Context context, List<Event> eventList) {
+        this.context = context;
+        this.eventList = eventList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(context)
+                .inflate(R.layout.item_event, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Event event = eventList.get(position);
+        holder.name.setText(event.getEventName());
+        holder.location.setText("Location: " + event.getLocation());
+        holder.date.setText("Date: " + event.getDate());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent i = new Intent(context, EventDetailsActivity.class);
+            i.putExtra("eventName", event.getEventName());
+            i.putExtra("location", event.getLocation());
+            i.putExtra("date", event.getDate());
+            context.startActivity(i);
+        });
+    }
+
+    @Override
+    public int getItemCount() { return eventList.size(); }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name, location, date;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.eventName);
+            location = itemView.findViewById(R.id.eventLocation);
+            date = itemView.findViewById(R.id.eventDate);
+        }
+    }
+}
---
+++ EntrantScreenActivity/app/src/main/java/com/example/entrantscreenactivity/views/EventDetailsActivity.java
@@ -0,0 +1,51 @@
+package com.example.entrantscreenactivity.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import com.example.entrantscreenactivity.R;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    TextView title, location, date;
+
+    Button joinLeaveButton;
+    boolean isJoined = false; // temporary state (later will come from Firebase)
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+
+        title = findViewById(R.id.title);
+        location = findViewById(R.id.location);
+        date = findViewById(R.id.date);
+
+        title.setText(getIntent().getStringExtra("eventName"));
+        location.setText("Location: " + getIntent().getStringExtra("location"));
+        date.setText("Date: " + getIntent().getStringExtra("date"));
+
+
+
+        joinLeaveButton = findViewById(R.id.joinLeaveButton);
+
+        // Default state text
+        updateButtonUI();
+
+        joinLeaveButton.setOnClickListener(v -> {
+            isJoined = !isJoined;   // toggle state
+            updateButtonUI();
+        });
+
+    }
+
+    private void updateButtonUI() {
+        if (isJoined) {
+            joinLeaveButton.setText("Leave Waiting List");
+        } else {
+            joinLeaveButton.setText("Join Waiting List");
+        }
+    }
+}
