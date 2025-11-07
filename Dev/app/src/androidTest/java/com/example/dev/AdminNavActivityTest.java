e05b8817e388e3f196b442112769cfe28d9485a1
teseraB <tesera@ualberta.ca>
2025-11-06 23:57:27 -0700

    Edit comment

--- Dev/app/src/androidTest/java/com/example/dev/AdminNavActivityTest.java
+++ Dev/app/src/androidTest/java/com/example/dev/AdminNavActivityTest.java
@@ -6,7 +6,6 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.Espresso.onIdle;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
