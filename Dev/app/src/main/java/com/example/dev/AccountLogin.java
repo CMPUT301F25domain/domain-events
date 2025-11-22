765204c94dbb2397162b57e1dbd88b6540506ea1
Kei-Iwashita <kei.iwashita@gmail.com>
2025-11-21 18:44:24 -0700

    commit

--- Dev/app/src/main/java/com/example/dev/AccountLogin.java
+++ Dev/app/src/main/java/com/example/dev/AccountLogin.java
@@ -3,6 +3,7 @@ package com.example.dev;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +13,22 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.dev.admin.AdminNavActivity;
+import com.example.dev.entrant.EntrantMainActivity;
+import com.example.dev.organizer.OrganizerDashboardActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
+
 public class AccountLogin extends AppCompatActivity {
 
     private EditText usernameEditText, passwordEditText;
     private Button loginBtn;
     private TextView signupRedirectText;
+    private FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +40,52 @@ public class AccountLogin extends AppCompatActivity {
         passwordEditText = findViewById(R.id.ET_password);
         loginBtn = findViewById(R.id.btn_login);
         signupRedirectText = findViewById(R.id.TV_signup_redirect);
+        database = FirebaseFirestore.getInstance();
 
 
         loginBtn.setOnClickListener(view -> {
-            String username = usernameEditText.getText().toString().trim();
-            String password = passwordEditText.getText().toString().trim();
+            String inputUsername = usernameEditText.getText().toString().trim();
+            String inputPassword = passwordEditText.getText().toString().trim();
 
-            if (TextUtils.isEmpty(username)) {
+            if (TextUtils.isEmpty(inputUsername)) {
                 usernameEditText.setError("Username is required");
                 return;
             }
 
-            if (TextUtils.isEmpty(password)) {
+            if (TextUtils.isEmpty(inputPassword)) {
                 passwordEditText.setError("Password is required");
                 return;
             }
 
-            // TODO: Authenticate account and delegate to org,admin or entrant screen
-            Toast.makeText(AccountLogin.this, "Logged in as: " + username, Toast.LENGTH_SHORT).show();
+            database.collection("accounts").whereEqualTo("username", inputUsername).get().addOnSuccessListener(querySnapshot -> {
+                if (querySnapshot.isEmpty()) {
+                    Toast.makeText(AccountLogin.this, "Username or password is incorrect", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                DocumentSnapshot doc = querySnapshot.getDocuments().get(0);
+                String storedPassword = doc.getString("password");
+                int accountType = doc.getLong("account type").intValue();
+
+                if (storedPassword != null && storedPassword.equals(inputPassword)) {
+                    if (accountType == 1) {
+                        startActivity(new Intent(AccountLogin.this, OrganizerDashboardActivity.class));
+                    }
+                    else if (accountType == 2) {
+                        startActivity(new Intent(AccountLogin.this, EntrantMainActivity.class));
+                    }
+                    else if (accountType == 3) {
+                        startActivity(new Intent(AccountLogin.this, AdminNavActivity.class));
+                    }
+                }
+                else {
+                    Toast.makeText(AccountLogin.this, "Username or password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
 
         signupRedirectText.setOnClickListener(view -> {
-            Intent intent = new Intent(AccountLogin.this, AccountSignup.class);
-            startActivity(intent);
+            startActivity(new Intent(AccountLogin.this, AccountSignup.class));
         });
     }
 }
--- Dev/app/src/main/java/com/example/dev/AccountSignup.java
+++ Dev/app/src/main/java/com/example/dev/AccountSignup.java
@@ -15,14 +15,24 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.dev.admin.AdminNavActivity;
+import com.example.dev.entrant.EntrantMainActivity;
+import com.example.dev.organizer.CreateEventActivity;
+import com.example.dev.organizer.FirebaseEvent;
+import com.example.dev.organizer.OrganizerDashboardActivity;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class AccountSignup extends AppCompatActivity {
     private Spinner accountSpi;
-    private EditText clearanceEditText, usernameEditText, passwordEditText;
+    private EditText clearanceEditText, usernameEditText, passwordEditText, confirmPasswordEditText;
     private Button signupBtn;
     private TextView loginRedirectText;
+    private FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +43,20 @@ public class AccountSignup extends AppCompatActivity {
         clearanceEditText = findViewById(R.id.ET_clearance_password);
         usernameEditText = findViewById(R.id.ET_username);
         passwordEditText = findViewById(R.id.ET_password);
+        confirmPasswordEditText = findViewById(R.id.ET_confirm_password);
         signupBtn = findViewById(R.id.btn_signup);
         loginRedirectText = findViewById(R.id.TV_login_redirect);
+        database = FirebaseFirestore.getInstance();
 
         List<String> accountTypes = Arrays.asList("Entrant", "Admin", "Organizer");
 
+        String adminClearance = "admin clearance";
+        String organizerClearance = "organizer clearance";
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_spinner_item, accountTypes
+                this, R.layout.item_spinner_default, accountTypes
         );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.item_spinner_dropdown);
         accountSpi.setAdapter(adapter);
 
         accountSpi.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -62,20 +77,43 @@ public class AccountSignup extends AppCompatActivity {
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = usernameEditText.getText().toString().trim();
-                String password = passwordEditText.getText().toString().trim();
+                String inputUsername = usernameEditText.getText().toString().trim();
+                String inputPassword = passwordEditText.getText().toString().trim();
+                String confirmPassword = confirmPasswordEditText.getText().toString().trim();
 
-                if (TextUtils.isEmpty(username)) {
+                if (TextUtils.isEmpty(inputUsername)) {
                     usernameEditText.setError("Username is required");
                     return;
                 }
-                if (TextUtils.isEmpty(password)) {
+                if (TextUtils.isEmpty(inputPassword)) {
                     passwordEditText.setError("Password is required");
                     return;
                 }
+                if (!inputPassword.equals(confirmPassword)) {
+                    confirmPasswordEditText.setError("Passwords do not match");
+                    return;
+                }
 
-                // TODO: Check username duplicates
-                Toast.makeText(AccountSignup.this, "Logged in as: " + username, Toast.LENGTH_SHORT).show();
+
+
+                database.collection("accounts").whereEqualTo("username", inputUsername).get().addOnSuccessListener(querySnapshot -> {
+                    if (!querySnapshot.isEmpty()) {
+                        Toast.makeText(AccountSignup.this, "Username already exists", Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Toast.makeText(AccountSignup.this, "Finna add account", Toast.LENGTH_LONG).show();
+                        DocumentReference newAccountRef = database.collection("accounts").document();
+                        String accountId = newAccountRef.getId();
+
+                        FirebaseAccount newAccount = new FirebaseAccount(accountId,2, inputPassword, inputUsername);
+
+                        newAccountRef.set(newAccount).addOnSuccessListener(aVoid -> {
+                            Toast.makeText(AccountSignup.this, "Account '" + inputUsername + "' created successfully", Toast.LENGTH_LONG).show();
+                        }).addOnFailureListener(e -> {
+                            Toast.makeText(AccountSignup.this, "Error registering account", Toast.LENGTH_LONG).show();
+                        });
+                    }
+                });
             }
         });
 
