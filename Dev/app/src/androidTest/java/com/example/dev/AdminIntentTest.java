6c5f911151ffe751615e955d8fa0216dbc630552
teseraB <tesera@ualberta.ca>
2025-11-30 15:54:25 -0700

    Create runnable tests for admin

--- Dev/app/src/androidTest/java/com/example/dev/AdminIntentTest.java
+++ Dev/app/src/androidTest/java/com/example/dev/AdminIntentTest.java
@@ -19,7 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class AdminNavActivityTest {
+public class AdminIntentTest {
 
     @Rule
     public ActivityScenarioRule<AdminNavActivity> activityRule =
