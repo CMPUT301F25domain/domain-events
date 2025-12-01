0efb0f397db83c579db79c4228afcab7ab6d7fe8
Kei-Iwashita <kei.iwashita@gmail.com>
2025-12-01 16:06:32 -0700

    delete main

--- Dev/app/src/main/java/com/example/dev/Keys.java
+++
@@ -1,8 +0,0 @@
-package com.example.dev;
-
-public final class Keys {
-    private Keys() {}
-    public static final String DEMO_EVENT_ID = "demo-event-001";
-    // optional extras:
-    // public static final String EXPORT_DIR = "exports";
-}
